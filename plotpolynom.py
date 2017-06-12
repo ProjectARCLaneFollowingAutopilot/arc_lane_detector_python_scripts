11853c333031afddfb07af6f64edff4f43a87ea5
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-12 23:45:44 +0200

    Working Scripts for visualising RANSAC

--- plotpolynom.py
+++ plotpolynom.py
@@ -9,14 +9,17 @@ b = 1
 c = 4
 
 # Polynomial using the LSQ fit.
-'''
-a_lsq = 
-b_lsq = 
-c_lsq = 
-'''
+
+a_lsq = 3.79
+b_lsq = 1.8
+c_lsq = -49
+
+# x_achse
+x_achse = np.linspace(-10.0, 10.0, 100)
+
+# Create variables to store the original, noisy data set.
 x = []
 y_noise = []
-y_noise
 
 # Read in the noisy data set.
 f = open('/home/nikhilesh/DataTextFiles/data.txt', 'r')
@@ -26,8 +29,16 @@ for line in f:
 	y_noise.append(currentline[1])
 f.close()
 
+# Sample the original polynomial.
+y_orig = a*x_achse*x_achse + b*x_achse + c
+
+# Sample the LSQ fitted polynomial.
+y_lsq = a_lsq*x_achse*x_achse + b_lsq*x_achse + c_lsq
+
 # Plot the data.
 plt.plot(x, y_noise, 'ro')
+plt.plot(x, y_orig, 'b')
+plt.plot(x, y_lsq, 'g')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.show()
